0ec19ac0b9295e8b65747db2abe7235930539fe9
teruteru <tutituti3080@gmail.com>
2023-03-01 08:04:05 +0900

    Add: 反復処理の最初まで

--- 060_関数とオブジェクト２/100_クラス継承/start/main.js
+++ 060_関数とオブジェクト２/100_クラス継承/start/main.js
@@ -1,25 +1,50 @@
-function Person(name, age) {
+class Person {
+  constructor(name, age) {
   this.name = name;
   this.age = age;
+  }
+  hello() {
+    console.log('hello ' + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// Person.prototype.hello = function() {
+//   console.log('hello ' + this.name);
+// }
+
+class Japanese extends Person {
+  constructor(name, age, gender) {
+    super(name, age);
+    this.gender = gender;
+  }
+  hello() {
+    console.log('Konnichiwa ' + this.name);
+  }
+  bye() {
+    console.log('Sayonara ' + this.name);
+  }
 }
 
-function Japanese(name, age, gender) {
-  Person.call(this, name, age);
-  this.gender = gender;
-}
+// function Japanese(name, age, gender) {
+//   Person.call(this, name, age);
+//   this.gender = gender;
+// }
 
-Japanese.prototype = Object.create(Person.prototype);
+// Japanese.prototype = Object.create(Person.prototype);
 
-Japanese.prototype.hello = function() {
-  console.log('Konnichiwa ' + this.name);
-}
+// Japanese.prototype.hello = function() {
+//   console.log('Konnichiwa ' + this.name);
+// }
 
-Japanese.prototype.bye = function() {
-  console.log('Sayonara ' + this.name);
-}
+// Japanese.prototype.bye = function() {
+//   console.log('Sayonara ' + this.name);
+// }
 
 const taro = new Japanese('Taro', 23, 'Male');
+console.log(taro);
+taro.bye();
